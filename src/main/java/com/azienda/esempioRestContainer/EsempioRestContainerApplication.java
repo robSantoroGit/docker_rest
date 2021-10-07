6db88f745893557171146b836e9042d1e30b196d
robsantoroGit <robesantoro@gmail.com>
2021-10-07 11:21:44 +0200

    primo commit

---
+++ src/main/java/com/azienda/esempioRestContainer/EsempioRestContainerApplication.java
@@ -0,0 +1,21 @@
+package com.azienda.esempioRestContainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EsempioRestContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsempioRestContainerApplication.class, args);
+	}
+	
+	@GetMapping("/ciao")
+	public String ciao() {
+		return "Ciao";
+	}
+
+}
---
+++ src/test/java/com/azienda/esempioRestContainer/EsempioRestContainerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.azienda.esempioRestContainer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsempioRestContainerApplicationTests {
+
+	@Test
+	public void test1() {
+		assertEquals(4,2+2);
+	}
+
+}
